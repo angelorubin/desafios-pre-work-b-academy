b3efc8f1235568f047f3077ea76e906b40061eb6
Angelo Rubin <angelorubin@gmail.com>
2021-08-20 11:44:31 -0300

    Resolução Desafio Dia 02 - Exercício 01 - Imutabilidade

--- dia2/src/main.js
+++ dia2/src/main.js
@@ -1,1 +1,14 @@
+const john = {
+  name: 'John',
+  surname: 'Doe',
+  age: 30,
+  hobbies: ['Surf', 'Design'],
+}
 
+const jane = Object.assign({}, john)
+
+jane.name = 'Jane'
+jane.hobbies = ["MuayThai", "Programming"];
+
+console.log('John:', john)
+console.log('Jane:', jane)
