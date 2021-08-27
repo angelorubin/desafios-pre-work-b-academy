e24921188048f4a4731496539abeee5902c67f5f
Angelo Rubin <angelorubin@gmail.com>
2021-08-27 17:25:17 -0300

    Exibindo items na tabela no carregamento da pagina

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,12 +1,29 @@
 import "./style.css";
-import { getCars } from "./services";
+import { createCar, retrieveCars } from "./services";
+import { checkElementExists } from "./utils";
 
+const container = document.querySelector("#container");
 const carForm = document.querySelector("#car-form");
-const registerBtn = document.querySelector("#register-btn");
+const carFormBtn = document.querySelector("#car-form__btn");
 const carsTable = document.querySelector("#cars-table");
 const carFormInputs = document.querySelectorAll("#car-form input");
+const message = document.querySelector("#message");
 
-carsTable.style.cssText = `display: none`;
+const hideElementById = (id) => {
+  const el = document.querySelector(`#${id}`);
+  return (el.style.display = "none");
+};
+
+const showElementById = (id) => {
+  const el = document.querySelector(`#${id}`);
+  return (el.style.display = "");
+};
+
+hideElementById("cars-table");
+
+container.style.cssText = `
+  margin: 0.5rem;
+`;
 
 const addRow = (values) => {
   let newRow = carsTable.insertRow(-1);
@@ -16,51 +33,61 @@ const addRow = (values) => {
     let newText = document.createTextNode(value);
     newCell.appendChild(newText);
   });
-
-  carForm.reset();
-
-  carsTable.style.cssText = `
-      margin-top: 10px;
-      border: 1px solid black;
-      display: "";
-    `;
 };
 
-registerBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+carFormBtn.addEventListener("click", (e) => {
+  carsTable.style.cssText = `display: none`;
 
-  let values = [];
+  const form = new FormData(carForm);
 
-  carFormInputs.forEach((data) => values.unshift(data.value));
+  const formData = Object.fromEntries(form);
 
-  addRow(values);
+  createCar(formData).then((res) => console.log(res));
 });
 
-getCars().then((data) => {
-  // console.log(data);
+retrieveCars()
+  .then((cars) => {
+    console.log("CARROS CADASTRADOS:", cars.data.length);
+    console.log(typeof cars);
+    // console.log(cars.data);
 
-  if (data.length > 0) {
-    carsTable.style.cssText = `display: ''`;
+    if (cars.data.length === 0) {
+      carsTable.style.cssText = `display: none`;
+      container.insertAdjacentHTML(
+        "beforeend",
+        "<p id='message'>Nenhum carro cadastrado.</p>"
+      );
+    } else {
+      carsTable.style.cssText = `
+        border: 1px solid #000;
+        margin-top: 1rem;
+    `;
 
-    let newRow = carsTable.insertRow(-1);
-    data.map((v, i) => {
-      for (let prop in v) {
-        console.log(prop);
-      }
+      cars.data.map((car, index) => {
+        let row = carsTable.insertRow(-1);
+        const cell5 = row.insertCell(0);
+        const cell4 = row.insertCell(0);
+        const cell3 = row.insertCell(0);
+        const cell2 = row.insertCell(0);
+        const cell1 = row.insertCell(0);
 
-      /*
-      let newRow = carsTable.insertRow(-1);
-      let newCell = newRow.insertCell(index);
-      let newText = document.createTextNode(data[index].);
-      newCell.appendChild(newText);
-      */
-    });
+        const image = document.createElement("img");
+        image.setAttribute("src", car.url);
+        image.style.cssText = `height: 100px`;
 
-    /*
-    let newRow = carsTable.insertRow(-1);
-    let newCell = newRow.insertCell(0);
-    let newText = document.createTextNode(data.image);
-    newCell.appendChild(newText);
-    */
-  }
-});
+        let brandModel = document.createTextNode(car.brandModel);
+        let year = document.createTextNode(car.year);
+        let plate = document.createTextNode(car.plate);
+        let color = document.createTextNode(car.color);
+
+        cell1.appendChild(image);
+        cell2.appendChild(brandModel);
+        cell3.appendChild(year);
+        cell4.appendChild(plate);
+        cell5.appendChild(color);
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
